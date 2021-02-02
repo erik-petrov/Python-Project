aaf13c12b973c85b2eefb34d9ce83a29ac981314
Erik Petrov <koteman123@hotmail.com>
2021-02-02 23:00:53 +0200

    Доделал лигу и укротитель ссылок, начал работу над картинками.

--- Final boss/Final_boss.py
+++ Final boss/Final_boss.py
@@ -1,20 +1,23 @@
 #TODO: PIL with file open, WeatherAPI, LOL info, link shortner(can be replaced)
 from tkinter import *
 from tkinter import ttk
-from tkinter import scrolledtext
 from tkinter.filedialog import *
 import sys, fileinput
 from tkinter.messagebox import *
 import requests
 import json
 import random
+import webbrowser
+from PIL import ImageTk, Image
 #https://
+
 def short():
+  try:
+    global url
     linkRequest = {
-      "destination": f"{ent.get()}"
+      "destination": f"https://{ent.get()}"
       , "domain": { "fullName": "rebrand.ly" }
       , "slashtag": f"itsshort_{random.randint(1,10000)*9999}"
-      , "title":"How to burn 10M"
     }
 
     requestHeaders = {
@@ -28,15 +31,39 @@ def short():
     print(r)
     if (r.status_code == requests.codes.ok):
         link = r.json()
-        print(link)
         print("Long URL was %s, short URL is %s" % (link["destination"], link["shortUrl"]))
+        url = link["shortUrl"]
+        label.config(text=url)
+  except:
+    label.config(text="Неверная ссылка.")
+
+def shortOpen(url):
+  webbrowser.open_new(url)
+
+def champInfo():
+  try:
+    champ = entC.get().capitalize()
+    r = requests.get(f"http://ddragon.leagueoflegends.com/cdn/11.2.1/data/ru_RU/champion/{champ}.json")
+    info = r.json()
+    cLabel.insert(0.0,info["data"][champ]["lore"])
+  except:
+    cLabel.insert(0.0,"Неверно введенные данные")
+
+def open_():
+  global filef
+  filef = askopenfilename()
+  print(filef)
+  photo = ImageTK.PhotoImage(Image.open(filef))
+  pilC.create_image(50,10,anchor=NW,image=photo)
+
+def rot():
+  pass
 
 main = Tk()
 main.geometry("600x600")
 main.title("something")
 
 tabs = ttk.Notebook(main)
-text=[1,2,3,4]
 
 tabs1 = Frame(tabs)
 tabs2 = Frame(tabs)
@@ -47,14 +74,31 @@ tabs.add(tabs2,text="League info")
 tabs.add(tabs3,text="PIL")
 tabs.add(tabs4,text="Weather")
 tabs.pack()
-
-w1 = Canvas(tabs1,width=600,height=600)
-w1.pack()
-
+#--------------укротитель ссылок-------------
+shortw = Canvas(tabs1,width=600,height=600)
+shortw.pack(expand=YES, fill=BOTH)
 ent = Entry(tabs1)
-w1.create_window(300,300,window=ent)
+shortw.create_window(300,300,window=ent)
+but = Button(tabs1,command=short,text="Сократить ссылку")
+shortw.create_window(300,340,window=but)
+label = Label(tabs1)
+shortw.create_window(300,200,window=label)
+#--------------инфа по лиге------------------
+leagueW = Canvas(tabs2,width=600,height=600)
+leagueW.pack(expand=YES, fill=BOTH)
+entC = Entry(tabs2)
+leagueW.create_window(300,530,window=entC)
+butt = Button(tabs2,text="Получить информацию о чемпионе.",command=champInfo)
+leagueW.create_window(300,560,window=butt)
+cLabel = Text(tabs2,width=60,height=30)
+leagueW.create_window(300,250,window=cLabel)
+#-------------------PIL---------------------
+pilC = Canvas(tabs3,width=600,height=600)
+pilC.pack(expand=YES, fill=BOTH)
+rotB = Button(tabs3,text="Повернуть",command=rot)
+pilC.create_window(300,560,window=rotB)
+openB = Button(tabs3,text="Открыть фото.",command=open_)
+pilC.create_window(300,540,window=openB)
 
-but = Button(tabs1,command=short)
-w1.create_window(300,320,window=but)
-
+label.bind("<Button-1>", lambda a: shortOpen(url))
 main.mainloop()
